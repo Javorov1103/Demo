21dcb8ccd3a854c7d6587594476948b1670a8918
Kalin <k.javorov@gmail.com>
2022-03-15 14:36:58 +0200

    Recursions

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, 6 };
+
+            var sum = SumLoop(numbers);
+
+            Console.WriteLine(sum);
+
+            var sum2 = SumRecursivly(numbers, 0);
+
+            Console.WriteLine(sum2);
+        }
+
+        public static int SumRecursivly(int[] arr, int index)
+        {
+            if(index == arr.Length)
+            {
+                return 0;
+            }
+
+            return arr[index] + SumRecursivly(arr, index + 1);
+        }
+
+        public static int SumLoop(int[] arr)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ Factorial/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("n = ");
+
+            int n = int.Parse(Console.ReadLine());
+
+            decimal factorial = Factorial(n);
+
+            Console.WriteLine("{0}! = {1}", n, factorial);
+        }
+
+        static decimal Factorial(int n)
+        {
+            // The bottom of the recursio
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            // Recursive call: the method calls itself
+            else
+            {
+                return n * Factorial(n - 1);
+            }
+
+        }
+    }
+}
